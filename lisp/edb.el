746481016068f80d54bee6b6a312d2239d34fe26
Jonas Bernoulli <jonas@bernoul.li>
2022-11-11 23:26:45 +0100

    Add edb.el
    Every package should come with a library
    whose name matches that of the package.

---
+++ lisp/edb.el
@@ -0,0 +1,31 @@
+;;; edb.el --- EDB, the Emacs Database
+
+;; Copyright (C) 2006,2007,2008 Thien-Thi Nguyen
+
+;; This file is part of EDB.
+;;
+;; EDB is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free
+;; Software Foundation; either version 3, or (at your option) any later
+;; version.
+;;
+;; EDB is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+;; for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with EDB; see the file COPYING.  If not, write to the Free
+;; Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+;; MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; EDB is the Emacs Database.
+
+;;; Code:
+
+(require 'database)
+(provide 'edb)
+
+;;; edb.el ends here
